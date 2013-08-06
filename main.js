75fb0061a199a2349ae46ff48951e8d1a05814d2
Ben <ben@unknown5c969d7163e7.gateway.2wire.net>
2013-08-05 18:44:49 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+//alert(localStorage[0]);
+// AVFW
+// Term 1308
+// Banchop Ben Kangdouangnhot
+
+
+// Wait until DOM is ready
+window.addEventListener( "DOMContentLoaded", function() {
+
+	// getElementById function
+	function  $( clear ) {
+	var ticks = document.getElementById( clear );
+	return ticks;
+}
+
+
+
+
+});
